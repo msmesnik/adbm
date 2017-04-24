2cc713250933e5caadea2efd98be162d72e55be1
Michael Smesnik <ms@tailored-apps.com>
2017-04-24 16:35:31 +0200

    implement all basic functionality

--- test/adbm.js
+++ test/adbm.js
@@ -1,6 +1,7 @@
 /* eslint-env mocha */
 
 const { expect } = require('chai')
+const adapter = require('adbm-mongodb')
 
 const { getConnection } = require('./helpers')
 const lib = require('../lib')
@@ -9,33 +10,15 @@ describe('migrations', function () {
   let db
   const mockInfoCollection = '_mocha_mock_migrations'
   const getIds = (items) => items.map(({ id }) => id)
-  const cleanup = async () => {
-    await db.collection(mockInfoCollection).drop()
-  }
 
   before(async () => {
     db = await getConnection()
   })
 
   describe('general functionality', function () {
-    before(async () => {
-      await db.collection(mockInfoCollection).insertMany([
-        { id: 'first', completed: new Date() },
-        { id: 'second', completed: new Date() }
-      ])
-    })
-    after(cleanup)
-
     it('requires a mongodb driver object', async function () {
       expect(() => lib.adbm()).to.throw()
       expect(() => lib.adbm(db)).to.not.throw()
-    })
-
-    it('gets a list of all completed migrations', async function () {
-      const ids = await lib.getCompletedMigrationIds(db, mockInfoCollection)
-
-      expect(ids).to.be.an('array')
-      expect(ids).to.have.all.members([ 'first', 'second' ])
     })
 
     it('requires migration objects to have "up" and "down" functions', function () {
@@ -62,21 +45,6 @@ describe('migrations', function () {
       expect(ids).to.have.all.members([ '01-ok', '03-ok' ])
       expect(ids).to.not.have.members([ '02-invalid', 'ignored', '04-ok' ])
     })
-
-    it('registers a successful migration', async function () {
-      const mockId = 'mock'
-      expect(getIds(await db.collection(mockInfoCollection).find().toArray())).to.not.contain(mockId)
-
-      await lib.registerMigration({ id: mockId, collection: mockInfoCollection, db })
-      expect(getIds(await db.collection(mockInfoCollection).find().toArray())).to.contain(mockId)
-    })
-
-    it('removes an entry from the list of performed migrations', async function () {
-      expect(getIds(await db.collection(mockInfoCollection).find().toArray())).to.contain('first')
-
-      await lib.unregisterMigration({ id: 'first', collection: mockInfoCollection, db })
-      expect(getIds(await db.collection(mockInfoCollection).find().toArray())).to.not.contain('first')
-    })
   })
 
   describe('running migrations', function () {
@@ -85,10 +53,10 @@ describe('migrations', function () {
     // const migrate = lib.adbm(db, { collection: mockInfoCollection, directory: 'test/mock' })
 
     before(() => {
-      migrate = lib.adbm(db, { collection: mockInfoCollection, directory: 'test/mock' })
+      migrate = lib.adbm({ db, adapter, metadata: mockInfoCollection, directory: 'test/mock' })
     })
     after(async () => {
-      await cleanup()
+      await db.collection(mockInfoCollection).drop()
       await db.collection(collection).drop()
     })
 
@@ -120,7 +88,7 @@ describe('migrations', function () {
         }
       ]
 
-      const ops = await lib.performMigrations({ migrations, direction: 'up', db, collection: mockInfoCollection, registerSuccess: noop })
+      const ops = await lib.performMigrations({ db, migrations, direction: 'up', metadata: mockInfoCollection, registerSuccess: noop })
 
       expect(ops).to.be.an('array')
       expect(ops.length).to.equal(2)
@@ -130,7 +98,7 @@ describe('migrations', function () {
     })
 
     it('applies pending migrations', async function () {
-      await migrate('up', { exclude: [ '03-ok' ] })
+      await migrate({ exclude: [ '03-ok' ] })
 
       const first = await db.collection(collection).find().toArray()
       expect(first.length).to.equal(2)
@@ -141,12 +109,14 @@ describe('migrations', function () {
     })
 
     it('reverts applied migrations', async function () {
-      await migrate('down', { exclude: [ '03-ok' ] })
+      const direction = 'down'
+
+      await migrate({ direction, exclude: [ '03-ok' ] })
 
       const first = await db.collection(collection).find().toArray()
       expect(first.length).to.equal(1)
 
-      await migrate('down')
+      await migrate({ direction })
       const second = await db.collection(collection).find().toArray()
       expect(second.length).to.equal(0)
     })
